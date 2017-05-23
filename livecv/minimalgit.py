a3c8a845e1b6de7bb55a73a842dbc9716966947b
Dinu SV <mail@dinusv.com>
2017-05-23 21:56:49 +0300

    Added Build and MinimalGit classes.

---
+++ livecv/minimalgit.py
@@ -0,0 +1,13 @@
+import os
+import platform
+from livecv.process import *
+
+class MinimalGit:
+
+    def __init__(self, repository):
+        self.repository = repository
+
+    def clone(self, branch = 'master', location = None, cwd = os.getcwd()):
+        location_path = [] if location == None else [os.path.abspath(location)]
+        proc = Process.run(['git', 'clone', '-b', branch, self.repository] + location_path, cwd)
+        Process.trace('GIT: ', proc, end = '')
