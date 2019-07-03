3a41c11d72f9baf9517a27a5103d39a238e17697
Milan Novak <33528453+herlocksholmes@users.noreply.github.com>
2019-07-03 17:55:53 +0200

    livepm install command (#1)
    Implemented install command

--- livepm/commands/__init__.py
+++ livepm/commands/__init__.py
@@ -6,6 +6,7 @@ from livepm.commands.license import LicenseCommand
 from livepm.commands.makedoc import MakeDocCommand
 from livepm.commands.new import NewCommand
 from livepm.commands.syncversion import SyncVersionCommand
+from livepm.commands.install import InstallCommand
 
 commands_order = [
     BuildCommand,
@@ -14,7 +15,8 @@ commands_order = [
     MakeDocCommand,
     NewCommand,
     SyncVersionCommand,
-    HelpCommand
+    HelpCommand,
+    InstallCommand
 ]  
 
 stored_commands = {c.name: c for c in commands_order}
---
+++ livepm/commands/install.py
@@ -0,0 +1,112 @@
+import sys
+import os
+import getopt
+import json
+import shutil
+import argparse
+import requests as re
+import urllib.parse
+import zipfile
+from livepm.lib.command import Command
+from livepm.lib.configuration import Configuration
+
+server_url = "https://livekeys.io/api"
+
+class InstallCommand(Command):
+
+    name = 'install'
+    description = 'Install a live package'
+
+    def __init__(self):
+        pass
+
+    def parse_args(self, argv):
+        parser = argparse.ArgumentParser(description='Install a live package')
+        parser.add_argument('--name', '-n', default=None, help='Name of the package')
+        parser.add_argument('--release', '-r', default=None, help='Package release')
+        parser.add_argument('--server_url', '-sU', default=server_url, help='Change server url.')
+        parser.add_argument('--install_globally', '-g' ,default=False, action='store_true', help="Install to livekeys dir")
+
+        args = parser.parse_args(argv)
+
+        self.name   = args.name
+        self.release   = args.release
+        self.server_url   = args.server_url
+  
+        # Directory construction
+        if args.install_globally:
+            try:
+                if os.environ['LIVEKEYS_DIR']:
+                    self.dir = os.environ["LIVEKEYS_DIR"]
+                    self.folder = 'plugins'
+            except KeyError:
+                print("Enviroment variable LIVEKEYS_DIR not set.")
+                sys.exit(1)
+        else:
+            self.dir = os.getcwd()
+            self.folder = 'packages'
+    
+    def __call__(self):
+        # Construct url
+        urlParams = 'package/' + self.name + '/' + 'latest/' + self.release
+        url = urllib.parse.urljoin(self.server_url, urlParams)
+        # Send request
+        r = re.get(url, allow_redirects=True)
+        
+        # Check the url
+        if r.ok:
+
+            # Package path construction
+            plugin_directory= os.path.join(self.dir, self.folder, self.name)
+            
+            # Create folders if installing in cwd
+            if os.environ != ["LIVEKEYS_DIR"]:
+                os.makedirs(plugin_directory)
+            else:
+                # os.makedirs(plugin_directory)
+                pass
+
+            jsonResponse = json.loads(r.text)
+            
+            # Request the zip file
+            getZip = re.get(jsonResponse['url'])
+            
+            # Read dependencies
+            versions = jsonResponse['dependencies']
+
+            # Download main package
+            package_path = os.path.join(plugin_directory, self.name + '-' + jsonResponse['version']  )
+            open( package_path + '.zip', 'wb').write(getZip.content)
+
+            # unzip main package and remove zip file
+            zipPath = (package_path + '.zip')
+            zip = zipfile.ZipFile(zipPath)
+            zip.extractall(package_path)
+            zip.close()
+            os.remove(package_path + '.zip')
+    
+            # Dependency recursive download
+            def downloadDependencies(versions):
+                
+                for i in versions:
+
+                    version = i['version']
+                    packageName = i['package']
+                    dependencyUrl = i['url']
+                    dependencyPath = os.path.join(plugin_directory, packageName['name'] + '-' + i['version'])
+
+                    # Download dependency
+                    open( dependencyPath + '.zip' , 'wb').write(dependencyUrl.content)
+
+                    # unzip main package and remove zip file
+                    zipPath = (dependencyPath + '.zip')
+                    zip = zipfile.ZipFile(zipPath)
+                    zip.extractall(dependencyPath)
+                    zip.close()
+                    os.remove(dependencyPath + '.zip')
+
+                    downloadDependencies(i['dependencies'])
+                    
+            downloadDependencies(versions)
+        else:
+            print("Package not found")
